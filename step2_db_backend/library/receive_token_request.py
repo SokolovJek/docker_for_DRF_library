a49fc9a5ad3e2f76c0f14975fafc862c0bed8096
SokolovJek <sokolovgek@mail.ru>
2022-04-23 12:23:05 +0300

    step2:2

--- step2_db_backend/library/receive_token_request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-response = requests.post('http://127.0.0.1:8000/api-token-auth/', data={'username': 'user1', 'password': 'geekshop'})
-print(response.status_code)     # 200
-print(response.json())         # {'token': 'f8004d8411aea2859e4e104fcffacd4272dea8fa'}
