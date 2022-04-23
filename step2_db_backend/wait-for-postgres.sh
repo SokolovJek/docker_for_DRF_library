0baef0871dd2ac701e558e0286c262329c992c26
SokolovJek <sokolovgek@mail.ru>
2022-04-23 11:48:41 +0300

    step2.2

--- step2_db_backend/wait-for-postgres.sh
+++ step2_db_backend/wait-for-postgres.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 # wait-for-postgres.sh
-# Скрипт в цикле будет пробовать подключаться к базе данных и, когда произойдёт удачное
-# подключение, совершит выход из бесконечного цикла
 
 set -e
 
@@ -9,9 +7,9 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD="dante123456" psql -h "$host" -d "library" -U "dante" -c '\q';>
->&2 echo "Postgres is unavailable - sleeping"
-sleep 1
+until PGPASSWORD="dante123456" psql -h "$host" -d "library" -U "dante" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
 done
 
 >&2 echo "Postgres is up - executing command"
