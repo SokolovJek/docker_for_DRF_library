ca2dc110a53092962247ea10d559e31821dd54d4
SokolovJek <sokolovgek@mail.ru>
2022-04-25 11:51:45 +0300

    step4_db_backend_gunicorn_nginx

--- step3_db_beckend_gunicorn/library/frontend/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-    return(
-    <div className='container footer '>
-         <p className="text-center">Компания © W3TODO. Все права защищены.</p>
-    </div>
-    )
-}
-export default Footer;
--- step3_db_beckend_gunicorn/library/todo/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from todo.models import ProjectModel, TodoModel
-
-
-admin.site.register(ProjectModel)
-admin.site.register(TodoModel)
