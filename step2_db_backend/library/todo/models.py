ef425070b0dbeffa41dad21771b4d651cab23a32
SokolovJek <sokolovgek@mail.ru>
2022-04-25 10:59:49 +0300

    step2.3

--- step2_db_backend/library/todo/models.py
+++ step2_db_backend/library/todo/models.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 
 class ProjectModel(models.Model):
     project_name = models.CharField(max_length=100)
-    users = models.ManyToManyField(Users)
+    users = models.ManyToManyField(Users, related_name='all users for project')
     link_git = models.URLField()
     descriptions = models.TextField()
     date_create = models.DateTimeField(auto_now_add=True)
@@ -20,13 +20,14 @@ class TodoModel(models.Model):
         ('DONE', 'done'),
     )
 
-    project = models.ForeignKey(ProjectModel, on_delete=models.CASCADE)
+    project = models.ForeignKey(ProjectModel, on_delete=models.CASCADE, related_name='todo for project')
     todo_descriptions = models.TextField()
     users = models.ForeignKey(Users,
                               on_delete=models.CASCADE,
+                              related_name='user_add_todo'
                               # models.SET_NULL,
-                              blank=True,
-                              null=True
+                              # blank=True,
+                              # null=True
                               )
     is_active = models.CharField(max_length=20, choices=STATUS_CHOICES, default='active')
     date_create = models.DateTimeField(auto_now_add=True)
